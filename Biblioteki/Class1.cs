ef2ce45b16c22aadcfd0107af619819362e7b27d
Michał Niekrasz <mnierasz@gmail.com>
2022-06-11 12:01:33 +0200

    init

---
+++ Biblioteki/Class1.cs
@@ -0,0 +1,41 @@
+﻿namespace Biblioteki;
+public class Class
+{
+    static string? path;
+    static int? number;
+    public static string GetPathOfFile(){
+        do{
+            Console.Write("Write here a path to file u want to read:  ");
+            path = Console.ReadLine();
+            
+            Console.Clear();
+        }
+        while(path==null||path=="");
+
+        return path;
+    }
+    public static int? GetNumberOfMinimalLengh(){
+        do{
+            
+            Console.Write("Write here a minimal lenght of the string u want to read:  ");
+            try{
+                string? s = Console.ReadLine();
+                if(s==null) continue;
+                number = int.Parse(s);
+            }
+            catch(FormatException){
+                Console.Clear();
+                number=null;
+                Console.WriteLine("podaj liczbę");
+            }
+            catch(OverflowException){
+                Console.Clear();
+                number=null;
+                Console.WriteLine("podaj mniejszą liczbę");
+            }
+        }
+        while(number==null);
+        
+        return number;
+    }
+}
---
+++ PrzeszukiwanieDowolnychPlikow/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using Biblioteki;
+
+static class Program{
+    static string output = "";
+    static string currentOutput = "";
+
+    static string? pathToFile;
+
+
+    static int? number = 4;
+    static int currentNumber = 0;
+    private static void Main(){
+        System.Console.Clear();
+        pathToFile = Class.GetPathOfFile();
+        number = Class.GetNumberOfMinimalLengh();
+
+        foreach (var item in File.ReadAllText(pathToFile))
+        {
+            if(item>32&&item<127){
+                currentNumber++;
+                currentOutput += item;
+            }
+            else{
+                if(currentNumber>=number){
+                    output+=currentOutput+"\n";                  
+                }
+                currentOutput = "";
+                number = 0;
+            }
+            
+        }
+        if(currentNumber>=number){
+            output+=currentOutput+"";    
+            currentOutput = "";
+            number = 0;              
+        }
+        System.Console.WriteLine(output);
+    }
+}
